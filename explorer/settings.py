9e822d56e5781c4278648a5c4434176c114d4e9f
Paul Downey <psd@whatfettle.com>
2017-05-31 23:18:33 +0100

    dj_database_url.config()

--- explorer/settings.py
+++ explorer/settings.py
@@ -78,24 +78,9 @@ WSGI_APPLICATION = 'explorer.wsgi.application'
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 
-def get_env_variable(var_name):
-    try:
-        return os.environ[var_name]
-    except KeyError:
-        error_msg = "Set the %s environment variable" % var_name
-    raise ImproperlyConfigured(error_msg)
+DATABASES = {}
+DATABASES['default'] =  dj_database_url.config()
 
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': get_env_variable('DATABASE_NAME'),
-        'USER': get_env_variable('DATABASE_USER'),
-        'PASSWORD': get_env_variable('DATABASE_PASSWORD'),
-        'HOST': '',
-        'PORT': '',
-    }
-}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
